2ed7504e50c1aded5f82dfb725488c2f56e67c2e
Eric Zanchi <emzanchi@gmail.com>
2017-01-04 14:25:21 -0600

    Added sidenav and tabs

--- src/js/Pages/Main.vue
+++ src/js/Pages/Main.vue
@@ -1,15 +1,35 @@
 <template lang="html">
   <div>
+    <!-- Title bar -->
     <md-toolbar>
-      <md-button class="md-icon-button">
+      <md-button class="md-icon-button" @click="toggleLeftSidenav">
         <md-icon>menu</md-icon>
       </md-button>
       <h2 class="md-title">Tape Recorder</h2>
     </md-toolbar>
-    <audio-list/>
-    <div class="record-holder">
-      <recorder/>
-    </div>
+
+    <!-- Side drawer -->
+    <md-sidenav class="md-left" ref="leftSidenav">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+    </md-sidenav>
+
+    <md-tabs md-centered md-fixed>
+      <!-- List of Recordings -->
+      <md-tab md-icon="list">
+        <audio-list/>
+      </md-tab>
+
+      <!-- Record tab -->
+      <md-tab md-icon="mic">
+        <div class="record-holder">
+          <recorder/>
+        </div>
+      </md-tab>
+    </md-tabs>
   </div>
 </template>
 <!--  -->
@@ -30,6 +50,9 @@ export default {
   methods: {
     changePageReciever(){
       console.log('test');
+    },
+    toggleLeftSidenav() {
+      this.$refs.leftSidenav.toggle();
     }
   },
   mounted(){},
