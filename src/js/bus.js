4ca20b777a8ab7bc2e7558ddcb2272fadada8695
Eric Zanchi <emzanchi@gmail.com>
2017-01-04 16:33:41 -0600

    Added new event type

--- src/js/bus.js
+++ src/js/bus.js
@@ -1,9 +1,10 @@
 // @flow
 import Vue from 'vue';
 
-const bus = new Vue();
+const bus = new Vue;
 
 // Events
 const dbupdate = 'dbupdate';
+const dbtrackremove = 'dbtrackremove';
 
 export {bus, dbupdate};
