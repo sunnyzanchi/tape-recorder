ed1af92c72ca3578dc57f3c8a34507ae133402f5
Eric Zanchi <emzanchi@gmail.com>
2017-01-17 09:16:24 -0600

    Updated serviceworker

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,31 +1,44 @@
 // @flow
-var cacheName: string = 'cache-v1';
-var urlsToCache: Array<string> = [
+const cacheName: string = 'cache-v1';
+const urlsToCache: Array<string> = [
   '/',
   '/css/main.css',
-  'js/main.bundle.js'
+  '/js/main.bundle.js',
+  '/manifest.json'
 ];
 
 /* Installation */
 self.addEventListener('install', function(e){
-  e.waitUntil(
-    caches.open(cacheName)
-    .then(function(cache){
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
-  );
+  e.waitUntil(setupCache);
 });
+
+function setupCache(){
+  caches.open(cacheName).then(function(cache){
+    return cache.addAll(urlsToCache);
+  });
+}
 
 /* Serving */
 self.addEventListener('fetch', function(e){
-  var p = caches.match(e.request);
-  var c = p.then(function(response){
-    if(response){
-      console.log(response);
-      return response;
-    }
-    return fetch(e.request);
-  });
-  e.respondWith(c);
+  // Serve immediately from cache
+  e.respondWith(fromCache(e.request));
+
+  // Update resources from network
+  e.waitUntil(update(e.request));
 });
+
+function fromCache(request){
+  return caches.open(cacheName).then(function(cache){
+    return caches.match(request).then(function(response){
+      return response || fetch(request);
+    });
+  });
+}
+
+function update(request){
+  return caches.open(cacheName).then(function(cache){
+    return fetch(request).then(function(response){
+      return cache.put(request, response);
+    });
+  });
+}
