e49c71fc9abd837ffcb8c5e4a59275494b825bfa
Eric Zanchi <emzanchi@gmail.com>
2017-03-21 09:42:13 -0500

    Removed unnecessary route

--- app.js
+++ app.js
@@ -7,20 +7,4 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-/* Routes */
-// Index
-app.get('/', function(req, res){
-  const options = {
-    root: path.join(__dirname, 'public')
-  };
-  res.sendFile('index.html', options, function(err){
-    if(err){
-      console.log(err);
-      res.status(err.status).end();
-    }
-  })
-});
-
-app.listen(3000, function(){
-  console.log('App started');
-});
+app.listen(process.env.PORT || 3000);
