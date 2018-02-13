1dc94ae2ac9c75433ba9fe587269c94c241f24e4
Zanchi <emzanchi@gmail.com>
2018-02-12 21:51:28 -0600

    Added action to retrieve tracks

--- src/js/store.js
+++ src/js/store.js
@@ -1,14 +1,20 @@
 /* eslint-disable no-param-reassign */
 import Vue from 'vue/dist/vue';
 import Vuex from 'vuex';
+import * as db from './db';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
+  actions: {
+    async getTracks({ commit }) {
+      commit('updateTracks', await db.getTracks());
+    },
+  },
   mutations: {
     updateTracks(state, tracks) {
       state.tracks = tracks;
-    }
+    },
   },
   state: {
     tracks: [],
