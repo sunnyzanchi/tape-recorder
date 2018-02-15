be1107808fea53b9cd15a7482ca5a0c97b65cdc1
Zanchi <emzanchi@gmail.com>
2018-02-14 23:09:20 -0600

    Added addTrack action

--- src/js/store/actions.js
+++ src/js/store/actions.js
@@ -1,6 +1,10 @@
 import * as db from '../db';
 
 export default {
+  async addTrack({ dispatch }, track) {
+    await db.addTrack(track);
+    dispatch('getTracks');
+  },
   async getTracks({ commit }) {
     commit('updateTracks', await db.getTracks());
   },
