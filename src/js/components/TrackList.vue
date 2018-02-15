22ede86cb8e3076247c27c891c53c187e07d0fc6
Zanchi <emzanchi@gmail.com>
2018-02-14 23:10:26 -0600

    Added basic display for tracks

--- src/js/components/TrackList.vue
+++ src/js/components/TrackList.vue
@@ -2,14 +2,20 @@
 
 </style>
 <template>
-  
+  <ul>
+    <li
+      v-for="track in tracks"
+      :key="track.name">
+      <p>{{ track.name }}</p>
+    </li>
+  </ul>
 </template>
 <script>
 import { mapState } from 'vuex';
 
 export default {
   computed: {
-    ...mapState(['tracks'])
-  }
-}
+    ...mapState(['tracks']),
+  },
+};
 </script>
