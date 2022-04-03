44920c5e6d131bd8d2f54639fc567b7af73f4331
Zanchi <zanchi@hey.com>
2022-04-03 01:49:47 -0400

    edit names in track list
    this commit adds the ability to click on
    track names in the list and edit them.
    also changes `title` in some places so
    everywhere is using `name` for consistency

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
+import cx from 'classnames'
+
 import TrackList from './TrackList'
 import useAudioEngine, { State } from './useAudioEngine'
 
-import db from '../db'
+import db, { Track } from '../db'
 
 if (process.env.NODE_ENV) {
   // @ts-expect-error
@@ -10,11 +12,26 @@ if (process.env.NODE_ENV) {
 
 const App = () => {
   const { play, status, toggleRecord, tracks } = useAudioEngine()
+  const recording = status === State.RECORDING
+
+  const changeName = (name: string, track: Track) => {
+    const newTrack = {
+      ...track,
+      name,
+    }
+    db.tracks.put(newTrack)
+  }
   return (
     <>
       {/* for debugging */}
       {process.env.NODE_ENV === 'development' && (
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            padding: '8px 24px',
+          }}
+        >
           <div>{State[status]}</div>
           <button onClick={() => db.deleteAll()} style={{ font: 'monospace' }}>
             rm -rf
@@ -22,9 +39,10 @@ const App = () => {
         </div>
       )}
 
-      <TrackList onPlay={play} tracks={tracks} />
-      <button class="record" onClick={toggleRecord}>
-        REC
+      <TrackList onPlay={play} onEditName={changeName} tracks={tracks} />
+      <button class={cx('record', { recording })} onClick={toggleRecord}>
+        <p>{recording ? 'STOP' : 'REC'}</p>
+        {recording && <div class="recording">Recording</div>}
       </button>
     </>
   )
--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -1,22 +1,68 @@
-import { FunctionalComponent } from 'preact'
 import { formatDuration } from 'date-fns'
+import { FunctionalComponent } from 'preact'
+import { useEffect, useRef, useState } from 'preact/hooks'
 
 import { Track } from '../db'
 import './TrackList.scss'
 
 type Props = {
   onPlay: (name: Track['name']) => unknown
+  onEditName: (name: string, track: Track) => void
   tracks: Track[]
 }
 
-const TrackList: FunctionalComponent<Props> = ({ onPlay, tracks }) => {
-  const play = (name: string) => () => onPlay(name)
+const TrackList: FunctionalComponent<Props> = ({
+  onEditName,
+  onPlay,
+  tracks,
+}) => {
+  const [editing, setEditing] = useState<string | null>(null)
+  const [name, setName] = useState<string>('')
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    if (editing) inputRef.current?.focus()
+  }, [editing, inputRef])
+
+  const editName = (track: Track) => () => {
+    setEditing(track.id)
+    setName(track.name)
+  }
+
+  const play = (id: string) => onPlay(id)
+
+  const saveChanges = (track: Track) => {
+    onEditName(name, track)
+    setEditing(null)
+    setName('')
+  }
 
   return (
     <ol class="track-list">
       {tracks.map((t) => (
-        <li key={t.id} onClick={play(t.id)}>
-          <p class="track-title">{t.name}</p>
+        <li
+          key={t.id}
+          onClick={(e) =>
+            // Don't play if we're clicking on the name to edit
+            !(e.target as HTMLElement).classList.contains('track-name') &&
+            play(t.id)
+          }
+        >
+          {editing === t.id ? (
+            <input
+              class="track-name"
+              onChange={(e) => setName(e.currentTarget.value)}
+              onFocus={() => inputRef.current?.select()}
+              onKeyDown={(e) => e.key === 'Enter' && saveChanges(t)}
+              ref={inputRef}
+              value={name}
+            />
+          ) : (
+            <p class="track-name" onClick={editName(t)}>
+              {t.name}
+            </p>
+          )}
+
           <p class="track-duration">
             {formatDuration(
               { seconds: Math.ceil(t.duration / 1000) },
